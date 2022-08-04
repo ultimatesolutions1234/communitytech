8225cdc7561860bffce7af26ac5653263cf3e5d5
Daniel Obembe <danielobembe1@gmail.com>
2022-08-04 21:30:21 +0100

    started adding the carousel to the couses section

---
+++ js/carousel.js
@@ -0,0 +1,28 @@
+const track = document.querySelector('.carousel__track');
+
+
+const slides = Array.from(track.children);
+
+const nextButton = document.querySelector('.carousel__button--right');
+
+const slideWidth = slides[0].getBoundingClientRect().width;
+
+// arange the slides next to each other
+const setSlidePosition = (slide, index) => {
+    slide.style.left = slideWidth * index + 'px';
+};
+slides.forEach(setSlidePosition);
+
+// When i click the right, move slide right;
+
+// nextButton.addEventListener('click', e => {
+//     const currentSLide = track.querySelector('.current-slide');
+//     const nextSlide = currentSLide.nextElementSibling;
+//
+//     const amountToMove = nextSlide.style,
+//         left;
+//     // move to the next slide
+//     track.style.transform = 'translateX(-' + amountToMove + ')';
+//     currentSLide.classList.remove('current-slide');
+//     nextSlide.classList.add('current-slide');
+// });
